65532e789db6bc0e09519255b14f5a51d28f56e0
VladSasnyk <sasnykvlad@gmail.com>
2024-03-25 12:48:06 +0200

    z-index fix

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,7 +8,7 @@ const Header = () => {
     }, [])
 
 
-    return <header className="w-full flex items-center justify-center fixed top-0 left-0">
+    return <header className="w-full flex items-center justify-center fixed top-0 left-0 z-50">
         <nav className="flex gap-4 bg-gray-200/20 p-1 rounded-xl mt-4 max-md:w-[90vw] items-center justify-center max-sm:w-[95vw] max-sm:gap-2" id='header-nav'>
             <NavItem to='/'>ABOUT</NavItem>
             <NavItem to='/skills'>SKILLS</NavItem>
